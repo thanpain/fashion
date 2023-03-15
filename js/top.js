3980404ac1a94d8cfb167cdb7532ef853e6fb240
Your Name <hoangvannguyen1311@gmail.com>
2023-03-15 18:01:42 +0700

    first commit

--- js/top.js
+++ js/top.js
@@ -1,3 +1,14 @@
+function preloader() {
+	$('#preloader').delay(0).fadeOut();
+};
+
+$(window).on('load', function () {
+	preloader();
+	mainSlider();
+	aosAnimation();
+	wowAnimation();
+});
+
 const offSet = 500; // khi kéo xuống 500px thì sẽ hiện nút top
 const time = 750; // sét cho nó mỗi lần trượt lên là 0.75s
 
